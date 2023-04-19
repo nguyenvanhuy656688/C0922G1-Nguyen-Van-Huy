4b66ee43dd2755e61aa8584d0834c2069003750c
nguyenvanhuy656688 <xpham9668@gmail.com>
2023-04-19 08:25:40 +0700

    submit

---
+++ spint2/aquarium/aquarium-fish-shop/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  @ViewChild('container') container: ElementRef;
+
+  constructor(private el: ElementRef) {
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSignInClick() {
+    this.container.nativeElement.classList.remove('right-panel-active');
+  }
+
+  onSignUpClick() {
+    this.container.nativeElement.classList.add('right-panel-active');
+  }
+}
